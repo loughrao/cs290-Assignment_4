0fe15faf2f26678288e3a19aee98916e5916ea2d
Owen Loughran <Owen Loughran>
2017-05-22 00:56:18 -0700

    Workin on it

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
-/*
- * Write your server code in this file.
- */
+var http = require("http");
+var fs = require('fs');
+
+console.log("PORT:", process.env.PORT);
+
+function requestHandler(req, res) {
+  console.log("method:", req.method);
+  console.log("url:", req.url);
+  console.log("headers:", req.headers);
+
+  // var radius = req.url.replace('/', '');
+  // console.log(radius);
+  // res.write("" + circle.area(radius));
+  // res.end();
+  var htmlContent = fs.readFileSync('public/index.html');
+
+
+  // res.statusCode = 200;
+  res.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  res.write(htmlContent);
+  res.end();
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(3000, function (err) {
+  console.log("Server running on port 3000");
+});
